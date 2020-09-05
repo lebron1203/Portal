56339d73317f4bd97ed436bd21132bc9ba4dbc3f
Francsico <lebron1203@gmail.com>
2020-09-04 21:28:27 -0400

    inicio de Proyecto

---
+++ sb-admin-2.Web/Clases/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Web.Clases
+{
+    public class Datos
+    {
+        public int Id { get; set; }
+
+        public string  Nombre { get; set; }
+
+        public Decimal Campo { get; set; }
+    }
+}
---
+++ sb-admin-2.Web/Clases/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Web.Clases
+{
+    public class Explosion
+    {
+
+        public int Id { get; set; }
+        public string Itemkey_sku { get; set; }
+        public string ItemKey { get; set; }
+        public string OrigenExcel { get; set; }
+        public string Mes_Inicial { get; set; }
+        public decimal Mes_1 { get; set; }
+        public decimal Mes_2 { get; set; }
+        public decimal Mes_3 { get; set; }
+        public decimal Mes_4 { get; set; }
+        public decimal Mes_5 { get; set; }
+        public decimal Mes_6 { get; set; }
+        public decimal Mes_7 { get; set; }
+        public decimal Mes_8 { get; set; }
+        public decimal Mes_9 { get; set; }
+        public decimal Mes_10 { get; set; }
+        public decimal Mes_11 { get; set; }
+        public decimal Mes_12 { get; set; }
+        public string Usuario_creacion{ get; set; }
+        public string Location{ get; set; }
+        public DateTime Fecha_creacion { get; set; }
+
+    }
+}
+
+
